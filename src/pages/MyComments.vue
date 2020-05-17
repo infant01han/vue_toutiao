d080ad8efd1adab23bde8370e5561408b6020505
Your Name <you@example.com>
2020-05-17 20:52:21 +0800

    添加我的跟帖功能

--- src/pages/MyComments.vue
+++ src/pages/MyComments.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
-      我的评论
+      <HeaderMiddle tilte="我的跟帖"></HeaderMiddle>
+      <comment :commentItem="item" :key="index" v-for="(item,index) in comments" />
     </div>
 </template>
 
 <script>
+    import comment from '@/components/comment'
+    import HeaderMiddle from "../components/HeaderMiddle"
     export default {
-        name: "MyComments"
+        components:{
+          comment,
+          HeaderMiddle
+        },
+      data(){
+          return {
+            comments: []
+          }
+      },
+      methods:{
+          getComments(){
+            this.$axios({
+              url:'/user_comments',
+              method:'get'
+            }).then(res=>{
+              console.log(res)
+              this.comments = res
+            })
+          }
+      },
+      mounted(){
+          this.getComments()
+      }
     }
 </script>
 
